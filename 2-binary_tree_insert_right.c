75101d928219263e6519a5c46cc71d840458f4d3
damukuzi <damukuzi@gmail.com>
2022-09-22 01:06:39 -0700

    adding changes

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -
- *
- * Return: whatever 
+ * binary_tree_insert_right - inserts right
+ *@parent: parent
+ *@value: value
+ * Return: whatever
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
